75a95e566f35766b0ed99d32032af7f3ae6a484f
astre <908112836@qq.com>
2021-02-01 10:14:22 +0800

    modified

--- download_g.py
+++ download_g.py
@@ -5,8 +5,11 @@ import os
 from astropy.io import fits
 
 
-CATALOG = "D:\\Code\\MachineLearning\\Data\\2020.12.15_MergerClassifier\\catalog\\"
-DATA = "D:\\Code\\MachineLearning\\Data\\2020.12.15_MergerClassifier\\raw_data\\"
+# CATALOG = "D:\\Code\\MachineLearning\\Data\\2020.12.15_MergerClassifier\\catalog\\"
+# DATA = "D:\\Code\\MachineLearning\\Data\\2020.12.15_MergerClassifier\\raw_data\\"
+
+CATALOG = "/mnt/d/Code/MachineLearning/Data/2020.12.15_MergerClassifier/catalog/"
+DATA = "/mnt/d/Code/MachineLearning/Data/2020.12.15_MergerClassifier/raw_data/"
 
 
 def createPackage(model_package: str):
@@ -49,30 +52,29 @@ def download_from_desi(fits: bool, jpg: bool, g: bool, r: bool, z: bool, data_na
         raise Exception("fits(boolean) cannot equal to jpg(boolean)")
     if fits:
         control += "fits"
-        save_dir += "fits\\"
+        save_dir += "fits/"
     if jpg:
         control += "jpeg"
-        save_dir += "jpg\\"
+        save_dir += "jpg/"
     control += "-cutout?ra=%f&dec=%f&layer=dr8&pixscale=%f&bands=" % (ra, dec, pix_scale)
     save_dir += data_name
     createPackage(save_dir)
     if g:
         control += "g' "
-        save_dir += "\\g"
+        save_dir += "/g"
     if r:
         control += "r' "
-        save_dir += "\\r"
+        save_dir += "/r"
     if z:
         control += "z' "
-        save_dir += "\\z"
+        save_dir += "/z"
     createPackage(save_dir)
-    url = "wget '" + control + save_dir + "\\" + "ra=%f&dec=%f&class=%s&dr7objid=%s&redshift=%f" \
+    url = "wget '" + control + "-O" + save_dir + "/" + "ra=%f-dec=%f-class=%s-dr7objid=%s-redshift=%f" \
           % (ra, dec, label, dr7objid, redshift)
     if fits:
         url += '.fits'
     if jpg:
         url += '.jpg'
-    print(url)
     os.system(url)
 
 
@@ -86,7 +88,7 @@ if __name__ == "__main__":
     dr7objid = fits_hdul.hdul[1].data[:]['dr7objid']
     redshift = fits_hdul.hdul[1].data[:]['redshift']
     if len(ra) == len(dec) == len(gz2_class) == len(dr7objid) == len(redshift):
-        for i in range(1):
+        for i in range(len(ra)):
             download_from_desi(fits=True, jpg=False, g=True, r=False, z=False,
                                data_name="all_redshift_under_0.1_galaxy_with_class",
                                pix_scale=0.262, ra=float(ra[i]), dec=float(dec[i]),
